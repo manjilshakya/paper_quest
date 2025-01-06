921e6b79a1a775812a3cfae1d1715f57e89ba6e7
Sandip <srai68141@gmail.com>
2025-01-06 22:29:26 +0545

    feat: created a state for storing user data after user is loggedin

--- src/app/tokenstore.ts
+++ src/app/tokenstore.ts
@@ -1,5 +1,6 @@
 ﻿import { devtools, persist } from "zustand/middleware";
 import {create} from "zustand/react";
+import { UserData, userData, UserDetailsState } from "./Models/Types";
 
 export interface Tokenstore{
     token: string;
@@ -17,6 +18,30 @@ const useTokenStore = create<Tokenstore>()(
         )
     )
 );
+
+export const useUserDetails = create<UserDetailsState>((set) => ({
+    email: "",
+    userId: 0,
+    name: "",
+    usertype: "",
+    isUserDataCompleted: false,
+    updateUserDetails: (userData: UserData) =>
+      set({
+        email: userData.email,
+        userId: typeof userData.userId === "string" ? parseInt(userData.userId, 10) : userData.userId,
+        name: userData.name,
+        usertype: userData.userType,
+        isUserDataCompleted: userData.isUserDataComplete === "1" || userData.isUserDataComplete === true,
+      }),
+    clearUserDetails: () =>
+      set({
+        email: "",
+        userId: 0,
+        name: "",
+        usertype: "",
+        isUserDataCompleted: false,
+      }),
+  }));
 
 
 export default useTokenStore;
