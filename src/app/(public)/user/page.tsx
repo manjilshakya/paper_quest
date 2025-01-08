2ac616a6959d6e66909a827ed92e4e8e6d0f2e93
Sandip <srai68141@gmail.com>
2025-01-08 22:29:15 +0545

    feat: used muatation and useref to send model data to the server

--- src/app/(public)/user/page.tsx
+++ src/app/(public)/user/page.tsx
@@ -1,157 +1,209 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, {FormEvent, useEffect, useRef, useState} from "react";
 import HomeNavbar from "./components/homeNavbar";
 import CustomerHome from "./home/customer";
 import Learn from "./home/Learn";
 import Modal from "./components/Modal";
-import useTokenStore, { useUserDetails } from "@/app/tokenstore";
-import { useRouter } from "next/navigation";
+import useTokenStore, {useUserDetails} from "@/app/tokenstore";
+import {useRouter} from "next/navigation";
+import {useMutation} from "@tanstack/react-query";
+import axios from "axios";
+import {UserModel} from "@/app/Models/Types";
 
 
 const page = () => {
-  // debugger;
-  const [isModalOpen, setModalOpen] = useState(false);
+    // debugger;
+    const [isModalOpen, setModalOpen] = useState(false);
 
-  const isUserDataCompleted = useUserDetails((state) => state.isUserDataComplete);
+    const isUserDataCompleted = useUserDetails((state) => state.isUserDataComplete);
 
-  const router = useRouter();
+  const institutionRef = useRef<HTMLInputElement>(null);
+  const phoneNumberRef = useRef<HTMLInputElement>(null);
+  const gradeRef = useRef<HTMLInputElement>(null);
+  const academicBackgroundRef = useRef<HTMLSelectElement>(null);
+  const semesterRef = useRef<HTMLSelectElement>(null);
 
-  useEffect(() => {
-    // Open the modal when the page loads
-    setModalOpen(true);
-  }, []);
+  const userId = useUserDetails((state) => state.userId);
 
-  const token = useTokenStore((state) => state.token);
 
-  if (token === "") {
-    console.log(token);
-    console.log("I am here");
-    router.push("/");
-  }
+    const router = useRouter();
 
-  useEffect(() => {
-    if (isUserDataCompleted) {
-      setModalOpen(false);
+    useEffect(() => {
+        // Open the modal when the page loads
+        setModalOpen(true);
+    }, []);
+
+    const token = useTokenStore((state) => state.token);
+
+    if (token === "") {
+        console.log(token);
+        console.log("I am here");
+        router.push("/");
     }
-  }, [isUserDataCompleted]);
 
-  const closeModal = () => setModalOpen(false);
-  return (
-    <div>
-      <Modal isOpen={isModalOpen} onClose={closeModal}>
-        <h2 className="text-xl font-bold mb-4">Welcome to the User Page!</h2>
-        <p className="text-gray-700">Intuition name</p>
-        <input type="text" />
-        <form>
-          {/* Institution Name */}
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 font-medium mb-2"
-              htmlFor="institution-name"
-            >
-              Institution Name
-            </label>
-            <input
-              type="text"
-              id="institution-name"
-              placeholder="Enter institution name"
-              className="w-full border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
+    useEffect(() => {
+        if (isUserDataCompleted) {
+            setModalOpen(false);
+        }
+    }, [isUserDataCompleted]);
 
-          {/* Phone Number */}
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 font-medium mb-2"
-              htmlFor="phone-number"
-            >
-              Phone Number
-            </label>
-            <input
-              type="text"
-              id="phone-number"
-              placeholder="Enter phone number"
-              className="w-full border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
+    const mutation = useMutation({
+      mutationFn: async (data : UserModel) => {
+        // Make the Axios request and set the headers
+        const response = await axios.post('http://localhost:5030/api/send-user-data', data, {
+          headers: {
+            'Content-Type': 'application/json', // JSON content type
+          },
+        });
+        return response.data;
+      },
+      onSuccess: (data) => {
+        console.log('data submitted successfully',data);
+      },
+      onError: (error) => {
+        console.log('error submitting data', error);
+      }
+    });
 
-          {/* Grade */}
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 font-medium mb-2"
-              htmlFor="grade"
-            >
-              Grade
-            </label>
-            <input
-              type="text"
-              id="grade"
-              placeholder="Enter your grade"
-              className="w-full border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
+    const handleSubmit = (e : FormEvent) => {
+      e.preventDefault();
+      const data: UserModel = {
+        userId: userId.toString(), // Assuming userId is already a number
+        institutionName: institutionRef.current?.value || '',
+        grade: '4', // Grade is hardcoded to 4 as per your example
+        semester: semesterRef.current?.value || '',
+        phoneNumber: phoneNumberRef.current?.value || '',
+        academicBackground: academicBackgroundRef.current?.value || '',
+      };
 
-          {/* Academic Background */}
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 font-medium mb-2"
-              htmlFor="academic-background"
-            >
-              Academic Background
-            </label>
-            <select
-              id="academic-background"
-              className="w-full border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            >
-              <option value="">Select an option</option>
-              <option value="arts">BICT</option>
-              <option value="science">MBA</option>
-            </select>
-          </div>
+      mutation.mutate(data);
+    }
 
-          {/* Optional Subject */}
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 font-medium mb-2"
-              htmlFor="optional-subject"
-            >
-              Semester
-            </label>
-            <select
-              id="optional-subject"
-              className="w-full border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            >
-              <option value="">Select an option</option>
-              <option value="first">First</option>
-              <option value="Second">Second</option>
-              <option value="third">Third</option>
-              <option value="fourth">Fourth</option>
-              <option value="fifth">Fifth</option>
-              <option value="sixth">Sixth</option>
-              <option value="seventh">Seventh</option>
-              <option value="eight">Eight</option>
-            </select>
-          </div>
+    const closeModal = () => setModalOpen(false);
+    return (
+        <div>
+            <Modal isOpen={isModalOpen} onClose={closeModal}>
+                <h2 className="text-xl font-bold mb-4">Welcome to the User Page!</h2>
+                <p className="text-gray-700">Institution name</p>
+                <input type="text"/>
+                <form>
+                    {/* Institution Name */}
+                    <div className="mb-4">
+                        <label
+                            className="block text-gray-700 font-medium mb-2"
+                            htmlFor="institution-name"
+                        >
+                            Institution Name
+                        </label>
+                        <input
+                            type="text"
+                            id="institution-name"
+                            placeholder="Enter institution name"
+                            className="w-full border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            ref={institutionRef}
+                        />
+                    </div>
 
-          {/* Submit Button */}
-          <div className="text-right">
-            <button
-              type="button"
-              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"
-              onClick={closeModal}
-            >
-              Submit
-            </button>
-          </div>
-        </form>
-      </Modal>
-      <HomeNavbar />
-      <CustomerHome />
-      <Learn />
+                    {/* Phone Number */}
+                    <div className="mb-4">
+                        <label
+                            className="block text-gray-700 font-medium mb-2"
+                            htmlFor="phone-number"
+                        >
+                            Phone Number
+                        </label>
+                        <input
+                            type="text"
+                            id="phone-number"
+                            placeholder="Enter phone number"
+                            className="w-full border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            ref={phoneNumberRef}
+                        />
+                    </div>
 
-    </div>
-  );
+                    {/* Grade */}
+                    <div className="mb-4">
+                        <label
+                            className="block text-gray-700 font-medium mb-2"
+                            htmlFor="grade"
+                        >
+                            Grade
+                        </label>
+                        <input
+                            type="text"
+                            id="grade"
+                            placeholder="Enter your grade"
+                            className="w-full border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            ref={gradeRef}
+                        />
+                    </div>
+
+                    {/* Academic Background */}
+                    <div className="mb-4">
+                        <label
+                            className="block text-gray-700 font-medium mb-2"
+                            htmlFor="academic-background"
+                        >
+                            Academic Background
+                        </label>
+                        <select
+                            id="academic-background"
+                            className="w-full border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            ref={academicBackgroundRef}
+                        >
+                            <option value="">Select an option</option>
+                            <option value="arts">BICT</option>
+                            <option value="science">MBA</option>
+                        </select>
+                    </div>
+
+                    {/* Optional Subject */}
+                    <div className="mb-4">
+                        <label
+                            className="block text-gray-700 font-medium mb-2"
+                            htmlFor="optional-subject"
+                        >
+                            Semester
+                        </label>
+                        <select
+                            id="optional-subject"
+                            className="w-full border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            ref={semesterRef}
+                        >
+                            <option value="">Select an option</option>
+                            <option value="first">First</option>
+                            <option value="Second">Second</option>
+                            <option value="third">Third</option>
+                            <option value="fourth">Fourth</option>
+                            <option value="fifth">Fifth</option>
+                            <option value="sixth">Sixth</option>
+                            <option value="seventh">Seventh</option>
+                            <option value="eight">Eight</option>
+                        </select>
+                    </div>
+
+                    {/* Submit Button */}
+                    <div className="text-right">
+                        <button
+                            type="button"
+                            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"
+                            onClick={(e) => {
+                              handleSubmit(e); // Call the handleSubmit function manually
+                              closeModal(); // Close the modal after submission
+                            }}
+                        >
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </Modal>
+            <HomeNavbar/>
+            <CustomerHome/>
+            <Learn/>
+
+        </div>
+    );
 };
 
 export default page;
